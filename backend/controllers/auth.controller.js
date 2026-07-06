be365021a674e8e8013a768daff7b69311e565b6
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-07-06 16:47:00 +0200

    project structure

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+  res.send("Signup route is working");
+}
+
+export const login = (req, res) => {
+  res.send("Login route is working");
+}
+
+export const logout = (req, res) => {
+  res.send("Logout route is working");
+}
