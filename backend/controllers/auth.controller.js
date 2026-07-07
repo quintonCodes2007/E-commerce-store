5943b84a3387eb16159ed4f783a66ab056377d2f
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-07-07 14:23:48 +0200

    create redis database and tokens

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,37 @@
-export const signup = (req, res) => {
-  res.send("Signup route is working");
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const generateTokens = (userId) => {
+  // created two different tokens, one for access and one for refresh
+  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
+  return { accessToken, refreshToken };
+};
+
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const userExists = await User.findOne({email});
+
+    if(userExists) {
+    return res.status(400).json({message: "User already exists"});  
+    }
+
+    const user = await User.create({name, email, password});
+
+    //authentication token generation logic
+    const { accessToken, refreshToken } = generateTokens(user._id);
+
+    res.status(201).json({user,message: "User created successfully",});
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
 }
 
-export const login = (req, res) => {
+export const login = async (req, res) => {
   res.send("Login route is working");
 }
 
-export const logout = (req, res) => {
+export const logout = async (req, res) => {
   res.send("Logout route is working");
 }
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
     email:{
         type: String,
         required: [true, "Email is required"],
-        unique: true
+        unique: true,
         lowercase:true,
         trim:true
     },
@@ -44,23 +44,22 @@ const userSchema = new mongoose.Schema({
 
 
 
-const User = mongoose.model("User", userSchema);
-
 // presave hook to hash password before saving to database
-userSchema.pre("save", async function(next) {
-    if(!this.isModified("password"))
-        return next();
+userSchema.pre("save", async function () {
+    if (!this.isModified("password"))
+        return;
     try{
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
-        next();
     } catch (error) {
-        next(error);
+        throw new Error("Error hashing password");
     }
 })
 
 userSchema.methods.comparePassword = async function(password) {
     return bcrypt.compare(password, this.password);
 }
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
