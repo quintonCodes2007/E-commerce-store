a69d298805eee8db12770fcf91f35871831b60e0
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-07-08 16:52:37 +0200

    added create product function with claudinary

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,5 @@
+import { redis } from '../lib/redis.js';
+import cloudinary from '../lib/cloudinary.js';
 import Product from '../models/product.model.js';
 
 export const getAllProducts = async (req, res) => {
@@ -35,4 +37,29 @@ export const getFeaturedProducts = async (req, res) => {
         res.status(500).json({message: "Server error", error:error.message});
     }
 
+}
+
+
+export const createProduct = async (req, res) => {
+    try {
+        const { name, description, price, image, category} = req.body;
+        let cloudinaryResponse = null;
+
+        if(image) {
+            cloudinaryResponse = await cloudinary.uploader.upload(image, {folder: "products"});
+        }
+
+        const product = await Product.create({
+            name,
+            description,
+            price,
+            image: cloudinaryResponse?.secure_url ?  cloudinaryResponse.secure_url : "",
+            category
+        });
+
+        res.status(201).json(product);
+    } catch (error) {
+        console.log("Error in createProduct controller:", error.message);
+        res.status(500).json({message: "Server error", error:error.message});
+    }
 }
