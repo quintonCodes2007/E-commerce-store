11c677716e448d4688cdeebfc13694e916065f91
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-07-08 16:30:35 +0200

    added isFeatured and isAdmin functions

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,38 @@
+import Product from '../models/product.model.js';
+
+export const getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find({});//find all products in the database
+        res.json({products});
+    } catch (error) {
+        console.log("Error in getAllProducts controller:", error.message);
+        res.status(500).json({message: "Server error", error:error.message});
+    }
+}; 
+
+export const getFeaturedProducts = async (req, res) => {
+    try{
+        let featuredProducts = await redis.get("featuredProducts");
+        if(featuredProducts) {
+            return res.json(JSON.parse(featuredProducts));
+        }
+
+        //if not in redis, fetch from database
+        //.lean() is used to return plain JavaScript objects instead of Mongoose documents, which can improve performance when you don't need the full Mongoose document functionality.
+        
+        featuredProducts = await Product.find({isFeatured: true}).lean();
+
+        if(!featuredProducts) {
+            return res.status(404).json({message: "No featured products found"});
+        }
+
+        //store in redis for 1 hour
+        await redis.set("featuredProducts", JSON.stringify(featuredProducts), "EX", 3600);
+
+        res.json(featuredProducts);
+    } catch(error){
+        console.log("Error in getFeaturedProducts controller:", error.message);
+        res.status(500).json({message: "Server error", error:error.message});
+    }
+
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import { getAllProducts, getFeaturedProducts } from '../controllers/product.controller.js';
+import { protectRoute, adminRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
-router.get("/",getAllProducts);
+
+router.get("/",protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
+router.post("/", protectRoute, adminRoute, createProduct);
+
 
 
 export default router;
