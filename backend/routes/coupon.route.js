060ee24e5cb1095efb3c2e6c905213c6d19f3cb3
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-07-09 15:54:56 +0200

    completed coupons routes

--- backend/routes/coupon.route.js
+++ backend/routes/coupon.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware';
 import { getCoupon } from '../controllers/coupon.controller';
+import { validateCoupon } from '../controllers/coupon.controller';
 
 const router = express.Router();
 
