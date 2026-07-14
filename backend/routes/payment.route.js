58afd1a43a87e5ce14af2f80b6a74a07f9b2f6d2
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-07-14 14:50:23 +0200

    added coupon functions

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js'
 import { createCheckoutSession } from '../controllers/payment.controller.js';
+import coupon from '../models/coupon.model.js';
+import { stripe } from '../lib/stripe.js';
 
 const router = express.Router();
 
@@ -17,13 +19,61 @@ router.post("/create-checkout-session", protectRoute, async (req, res) => {
 
         const lineItems = products.map(product=>  {
             const amount = Math.round(product.price * 100);// strip wants format of cents
+            totalAmount += amount * product.quantity
+
+            return{
+                price_data: {
+                    currency: "usd",
+                    product_data: {
+                        name: product.name,
+                        images: [product.image],
+                    },
+                    unit_amount: amount
+                },
+            }
             
+        });
+
+        let coupon = null;
+        if (couponCode) {
+            coupon = await coupon.findOne({code:couponCode, userId:req.user._id, isActive:true});
+            if (coupon){
+                totalAmount -= Math.round(totalAmount * coupon.discountPrecentage / 100);
+            }                 
+        }
+
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: lineItems,
+            mode: "payment",
+            success_url: `${process.env.CLIENT_URL}/purchase-success?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: `${process.env.CLIENT_URL}/purchase-cancel`,
+            discounts: coupon
+                ? [
+                    {
+                        coupon: await coupon.getStripeCoupon(coupon.discountPrecentage),
+                        
+                    },
+                  ]
+                : [],
         })
 
     } catch (error) {
         
     }
 });
+
+async function createStripeCoupon( discountPrecentage) {
+    const coupon = await stripe.coupons.create({
+        percent_off: discountPrecentage,
+        duration: "once"
+        
+      });
+
+      return coupon.id;
+}
+
+
 
 
 
