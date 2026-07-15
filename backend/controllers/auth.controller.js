e1fb0e6fa2308fa8fa786afc842c5a3ae23ac2b9
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-07-15 17:10:49 +0200

    finished with backend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -137,9 +137,9 @@ export const refreshToken = async (req, res) => {
 //TODO: implement get profile later
 export const getProfile = async (req, res) => {
   try {
-    
+    res.json(req.user);
   } catch (error) {
-    
+    res.status(500).json({message: "Server error", error: error.message});
   }
 }
 	
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import { signup, login, logout, refreshToken } from '../controllers/auth.controller.js';
+import { signup, login, logout, refreshToken, getProfile } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
+
 
 const router = express.Router();
 
