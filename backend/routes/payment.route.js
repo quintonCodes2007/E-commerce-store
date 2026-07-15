490a7db5b2307e018d2de9c1b5a73a29300da85b
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-07-15 15:00:57 +0200

    new order function

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,10 +1,45 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js'
 import { createCheckoutSession } from '../controllers/payment.controller.js';
+import { stripe } from '../lib/stripe.js';
 
 const router = express.Router();
 
 router.post("/create-checkout-session", protectRoute, createCheckoutSession);
+router.post("/checkout-success", protectRoute, async (req,res) => {
+    try {
+        const {sessionId} = req.body;
+        const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+        if(session.payment_status === 'paid'){
+            
+            if(session.metadata.couponCode){
+                await Coupon.findOneAndUpdate({
+                    code:session.metadata.couponCode, userId:session.metadata
+                }, {
+                    isActive:false
+                });
+            }
+
+            //create new order
+            const products = JSON.parse(session.metadata.products);
+            const newOrder = new Order({
+                userId: session.metadata.userId,
+                products: products.map(product => ({
+                    product: product.id,
+                    quantity: product.quantity,
+                    price: product.price
+                })),
+                totalAMount: session.total_amount / 100,
+                stripeSessionId: sessionId
+            }),
+
+            await newOrder.save();
+        }
+    } catch (error) {  
+        
+    }
+});
 
 export default router;
 
