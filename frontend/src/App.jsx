87287b4c12f533897d609dafc0005a2878f1b83b
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-07-16 14:00:11 +0200

    started with fron tend bacground gradient

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="layout-wrapper">
+      <div className="bg-glow-overlay"/>
+      <div className="relative z-10"/>
+      <div className="relative z-50 pt-20"/>
+
+      <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from 'lucide-react';
+import  { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
