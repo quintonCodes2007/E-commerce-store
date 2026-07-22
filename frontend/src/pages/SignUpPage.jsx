2b05cb5e00eb5bda0a5498abeed6714e66b04183
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-07-22 17:14:35 +0200

    added three more input fields

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -53,8 +53,71 @@ const SignUpPage = () => {
                 required
                 value={formData.name}
                 onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-                className='block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm'
+                className='block w-full pl-10 px-3 py-2 border border-gray-700 rounded-md focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm'
                 placeholder='Full name'
+              />
+            </div>
+            </div>
+
+
+           <div>
+            {/* <label htmlFor="name" className='block text-sm font-medium text-gray-300'>
+              Full name
+            </label> */}
+            <div className='mt-1 relative round-md shadow-sm'>
+              <div className='absolute inset-y-0 left-0 pl-3 flex items -center pointer-events-none'>
+                <Mail className='h-9 w-5 text-gray-400' aria-hidden='true' />
+              </div>
+              <input
+                id='email'
+                type='email'
+                required
+                value={formData.email}
+                onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                className='block w-full pl-10 px-3 py-2 border border-gray-700 rounded-md focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm'
+                placeholder='Email'
+              />
+            </div>
+            </div> 
+
+
+           <div>
+            {/* <label htmlFor="name" className='block text-sm font-medium text-gray-300'>
+              Full name
+            </label> */}
+            <div className='mt-1 relative round-md shadow-sm'>
+              <div className='absolute inset-y-0 left-0 pl-3 flex items -center pointer-events-none'>
+                <Lock className='h-9 w-5 text-gray-400' aria-hidden='true' />
+              </div>
+              <input
+                id='password'
+                type='password'
+                required
+                value={formData.password}
+                onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                className='block w-full pl-10 px-3 py-2 border border-gray-700 rounded-md focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm'
+                placeholder='Password'
+              />
+            </div>
+            </div> 
+
+
+           <div>
+            {/* <label htmlFor="name" className='block text-sm font-medium text-gray-300'>
+              Full name
+            </label> */}
+            <div className='mt-1 relative round-md shadow-sm'>
+              <div className='absolute inset-y-0 left-0 pl-3 flex items -center pointer-events-none'>
+                <Lock className='h-9 w-5 text-gray-400' aria-hidden='true' />
+              </div>
+              <input
+                id='confirmPassword'
+                type='password'
+                required
+                value={formData.name}
+                onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+                className='block w-full pl-10 px-3 py-2 border border-gray-700 rounded-md focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm'
+                placeholder='Confirm Password'
               />
             </div>
             </div> 
