f8427778220d68e5e792c34d3775c8dc3445ab8a
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-07-23 14:29:56 +0200

    added axios file

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,121 @@
-import React from 'react'
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+import { LogIn, Mail, Lock, ArrowRight, Loader } from 'lucide-react';
 
 const LoginPage = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const loading = false;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+
+  };
+
   return (
-                <button type='submit' onClick={() => console.log('BUTTON CLICKED')}
-            >Sign Up</button>
+
+    <div className='flex flex-col justify-center py-1 sm:px-6 lg:px-8'>
+      <motion.div
+      className='sm:mx-auto sm:w-full max-w-md'
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8 }}
+      >
+        <h2 className='mt-6 text-center text-3xl font-extrabold text-emerald-400 h-15'>Login </h2>
+
+        
+      </motion.div>
+
+      <motion.div
+      
+      className='sm:mx-auto sm:w-full max-w-md'
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8, delay: 0.1 }}
+      >
+        <div className='bg-gray-800 py-8 px-4 shadow sm:rounded-lg sm:px-10'>
+
+
+          
+
+          <form onSubmit={handleSubmit} className='space-y-6'>
+
+           <div>
+            {/* <label htmlFor="name" className='block text-sm font-medium text-gray-300'>
+              Full name
+            </label> */}
+            <div className='mt-1 relative rounded-md shadow-sm'>
+              <div className='absolute inset-y-0 left-0 pl-3 flex 
+              items-center pointer-events-none'>
+                <Mail className='h-5 w-5 text-gray-400' aria-hidden='true' />
+              </div>
+              <input
+                id='email'
+                type='email'
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className='block w-full pl-10 px-3 py-2 border border-gray-700 rounded-md focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm'
+                placeholder='Email'
+              />
+            </div>
+            </div> 
+
+           <div>
+            {/* <label htmlFor="name" className='block text-sm font-medium text-gray-300'>
+              Full name
+            </label> */}
+            <div className='mt-1 relative rounded-md shadow-sm'>
+              <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+                <Lock className='h-5 w-5 text-gray-400' aria-hidden='true' />
+              </div>
+              <input
+                id='password'
+                type='password'
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className='block w-full pl-10 px-3 py-2 border border-gray-700 rounded-md focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm'
+                placeholder='Password'
+              />
+            </div>
+            </div> 
+
+           <button 
+           type='submit' className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 transition duration-150 ease-in-out disabled:opacity-50' disabled={loading}
+           >
+            {loading ? (
+              <>
+                <Loader className='mr-2 h-5 w-5 animate-spin'aria-hidden='true' />
+                Loading...
+              </>
+              ) : (
+              <>
+                <LogIn className='mr-2 h-5 w-5' aria-hidden='true' />
+                Login
+              </>
+            )}
+            </button>   
+
+
+
+          </form>
+
+          <p className='mt-8 text-center text-sm text-gray-300'>
+            Not a member? {' '}
+            <Link to='/signup' className='font-medium text-emerald-400 hover:text-emerald-300'>
+             Sign up now <ArrowRight className='inline-block h-4 w-4'/>
+             </Link>
+          </p>
+
+        </div>
+      </motion.div>
+
+    </div>
+
   )
 }
 
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -128,7 +128,7 @@ const SignUpPage = () => {
             </div>
 
            <button 
-           type='submit' className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600 hover:bg-emerald-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 transition duration-150 ease-in-out disabled:opacity-50' disabled={loading}
+           type='submit' className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 transition duration-150 ease-in-out disabled:opacity-50' disabled={loading}
            >
             {loading ? (
               <>
