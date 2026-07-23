dae9e95fc15087a5df89f595e7dbd67d5785deb8
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-07-23 09:00:18 +0200

    spinning animation for loading button

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -27,14 +27,15 @@ const SignUpPage = () => {
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8 }}
       >
-        <h2 className='mt-6 text-center text-3xl font-extrabold text-emerald-400'>Create your account </h2>
+        <h2 className='mt-6 text-center text-3xl font-extrabold text-emerald-400 h-15'>Create your account </h2>
       </motion.div>
 
       <motion.div
+      
       className='sm:mx-auto sm:w-full max-w-md'
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.8, delay: 0.2 }}
+      transition={{ duration: 0.8, delay: 0.3 }}
       >
         <div className='bg-gray-800 py-8 px-4 shadow sm:rounded-lg sm:px-10'>
 
@@ -59,7 +60,6 @@ const SignUpPage = () => {
             </div>
             </div>
 
-
            <div>
             {/* <label htmlFor="name" className='block text-sm font-medium text-gray-300'>
               Full name
@@ -79,7 +79,6 @@ const SignUpPage = () => {
               />
             </div>
             </div> 
-
 
            <div>
             {/* <label htmlFor="name" className='block text-sm font-medium text-gray-300'>
@@ -101,7 +100,6 @@ const SignUpPage = () => {
             </div>
             </div> 
 
-
            <div>
             {/* <label htmlFor="name" className='block text-sm font-medium text-gray-300'>
               Full name
@@ -114,13 +112,30 @@ const SignUpPage = () => {
                 id='confirmPassword'
                 type='password'
                 required
-                value={formData.name}
-                onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+                value={formData.confirmPassword}
+                onChange={(e) => setFormData({ ...formData, confirmPassword: e.target.value })}
                 className='block w-full pl-10 px-3 py-2 border border-gray-700 rounded-md focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm'
                 placeholder='Confirm Password'
               />
             </div>
-            </div> 
+            </div>
+
+           <button 
+           type='submit' className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 transition duration-150 ease-in-out disabled:opacity-50' disabled={loading}
+           >
+            {loading ? (
+              <>
+                <Loader className='mr-2 h-5 w-5 animate-spin'aria-hidden='true' />
+                Loading...
+              </>
+              ) : (
+                <>
+                <UserPlus className='mr-2 h-5 w-5' aria-hidden='true' />
+                Sign Up
+                </>
+            )}
+            </button>   
+
           </form>
 
         </div>
