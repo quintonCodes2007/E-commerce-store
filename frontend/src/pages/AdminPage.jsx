38084e11a285b46609eca03bfe07a936a6666f3c
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-08-03 16:42:04 +0200

    adminpage fix

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -12,14 +12,7 @@ const AdminPage = () => {
                 Some artifacts choose their owner. The rest just scream until someone buys them.
             </p>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                {categories.map((category) => (
-                    <CategoryItem
-                        category={category}
-                        key={category.name}
-                    />
-                ))}
-            </div>
+
 
         </div>
     </div>
