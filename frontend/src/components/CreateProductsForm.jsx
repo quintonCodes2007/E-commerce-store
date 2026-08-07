b86e5bc5439a83bb624ef2dcf70e3fabd30a7239
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-08-07 09:59:48 +0200

    styling for create product form

--- frontend/src/components/CreateProductsForm.jsx
+++ frontend/src/components/CreateProductsForm.jsx
@@ -22,7 +22,7 @@ const CreateProductsForm = () => {
 
   return (
     <motion.div
-        className='bg-gray-800 shadow-lg rounded-lg p-8 max-w-xl mx-auto'
+        className='bg-red-300/10 shadow-lg rounded-lg p-8 max-w-xl mx-auto'
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
@@ -38,7 +38,7 @@ const CreateProductsForm = () => {
                     name='name'
                     value={newProduct.name}
                     onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
-                    className='mt-1 block w-full bg-gray-700 border-gray-600 rounded-md  shadow-sm py-2 px-3 focus:border-red-200 focus:ring-1 focus:ring-red-400 focus:outline-none mb-3'
+                    className='mt-1 block w-full border border-red-400/40 rounded-md  shadow-sm py-2 px-3 focus:border-red-400 focus:ring-1 focus:ring-red-500 focus:outline-none mb-3'
                     required placeholder='Product name'
                 />
             </div>
@@ -51,7 +51,7 @@ const CreateProductsForm = () => {
                     name='description'
                     value={newProduct.description}
                     onChange={(e) => setNewProduct({ ...newProduct, description: e.target.value })}
-                    className='mt-1 block w-full bg-gray-700 border-gray-600 rounded-md  shadow-sm py-2 px-3 focus:border-red-200 focus:ring-1 focus:ring-red-400 focus:outline-none mb-3'
+                    className='mt-1 block w-full border border-red-400/40 rounded-md  shadow-sm py-2 px-3 focus:border-red-400 focus:ring-1 focus:ring-red-500 focus:outline-none mb-3'
                     required placeholder='Product description'
                 />
             </div>
@@ -65,7 +65,7 @@ const CreateProductsForm = () => {
                     value={newProduct.price}
                     onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
                     step='0.01'
-                    className='mt-1 block w-full bg-gray-700 border-gray-600 rounded-md  shadow-sm py-2 px-3 focus:border-red-200 focus:ring-1 focus:ring-red-400 focus:outline-none mb-3'
+                    className='mt-1 block w-full border border-red-400/40 rounded-md  shadow-sm py-2 px-3 focus:border-red-400 focus:ring-1 focus:ring-red-500 focus:outline-none mb-3'
                     required placeholder='Product price'
                 />
             </div>
@@ -77,12 +77,12 @@ const CreateProductsForm = () => {
                     name='category'
                     value={newProduct.category}
                     onChange={(e) => setNewProduct({ ...newProduct, category: e.target.value })}
-                    className='mt-1 block w-full bg-gray-700 border-gray-600 rounded-md  shadow-sm py-2 px-3 focus:border-red-200 focus:ring-1 focus:ring-red-400 focus:outline-none mb-3'
+                    className='mt-1 block w-full border border-red-400/40 rounded-md  shadow-sm py-2 px-3 focus:border-red-400 focus:ring-1 focus:ring-red-500 focus:outline-none mb-3'
                     required
                 >
-                <option value=''>Select a category</option>
+                <option className="bg-gray-800 text-red-400" value=''>Select a category</option>
                 {categories.map((category) => (
-                    <option key={category} value={category}>
+                    <option key={category} value={category} className="bg-gray-900 text-red-400">
                         {category}
                     </option>
                 ))}
@@ -91,7 +91,7 @@ const CreateProductsForm = () => {
 
             <div className='mt-1 flex items-center'>
                 <input type="file" id='image' className='sr-only' accept='image/*' />
-                <label htmlFor="image" className='cursor-pointer bg-gray-700 py-2 px-3 border border-gray-600 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-300 hover:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-offset  focus:ring-red-500'
+                <label htmlFor="image" className='cursor-pointer  py-2 block w-full text-center border border-red-400 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-300 hover:bg-red-800/30 focus:outline-none focus:ring-1 focus:ring-offset  focus:ring-red-500'
                 >
                     <Upload className='h-5 w-5 inline-block mr-2' />
                     Upload an image
