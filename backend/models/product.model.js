7e611dd484b34010884bac45921063d8f79e47bd
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-08-18 23:20:21 +0200

    added product quantity to model

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -14,6 +14,11 @@ const productSchema = new mongoose.Schema({
         min: 0,
         required: true,
     },
+    inventoryQuantity: {
+        type: Number,
+        min: 0,
+        default: 0,
+    },
     image: {
         type: String,
         required: [true, 'Image is required']
